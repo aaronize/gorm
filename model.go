619730a508e555ae9eefc3fb83fa688131d13c33
AaronChen <chendai2004@hotmail.com>
2019-08-15 16:25:06 +0800

    Update model.go

--- model.go
+++ model.go
@@ -7,8 +7,8 @@ import "time"
 //      gorm.Model
 //    }
 type Model struct {
-	ID        uint `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time `sql:"index"`
+	ID        uint `json:"id" gorm:"primary_key column:id"`
+	CreatedAt time.Time `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"column:updated_at"`
+	DeletedAt *time.Time `json:"deleted_at" sql:"index" gorm:"column:deleted_at"`
 }
